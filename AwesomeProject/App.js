a8e8c9107401ddca4bb15674e590ff3867b9b704
Drimba Alexandru <drimba.alex@gmail.com>
2018-01-04 00:57:24 +0200

    Async call to update local DB and animated loading thingy

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -11,7 +11,8 @@ import {
   Text,
   View,
   Button,
-  Alert
+  Alert,
+  ActivityIndicator
 } from 'react-native';
 import Native_bridge_test from './javaModuleWrapper';
 
@@ -30,38 +31,69 @@ class TextThingy extends Component {
   }
 }
 
+class LoadingCircleContainer extends Component
+{
+	render()
+	{
+		let indicator = null;
+		if( !this.props.doneLoading )
+		{
+			indicator = <ActivityIndicator size="large" color="#0000ff" />;
+		}
+		else
+		{
+			indicator = <Text> </Text>
+		}
+		return ( 
+			<View>
+				{indicator}
+			</View>
+		);
+	}
+}
+
 export default class App extends Component<{}>
 {
+	
+	init() 
+	{
+		Native_bridge_test.isOnline( 
+			( isOnline ) =>
+			{
+				this.setState( {doneLoading: true } );
+				this.setState( {showtext: "Gata incarcat"});
+				if( isOnline==true )
+				{
+					Native_bridge_test.update_local(() =>
+					{
+					   alert( "Updated local DB");
+					},
+					( errmsg ) =>
+					{
+					   alert( errmsg );
+					});
+				}
+				else
+				{
+					alert( "The server is not available" );
+				}
+			},
+			( errmsg ) =>
+			{
+				this.setState( {doneLoading: true } );
+				this.setState( {showtext: "Gata incarcat"});
+				alert( errmsg );
+			});
+	}
+	
     constructor( props )
     {
         super( props );
         this.state = {showtext: 'man\'s not hot',
-                      dbReady : false };
-        Native_bridge_test.isOnline( ( isOnline ) =>
-                                      {
-                                          if(isOnline==true)
-                                          {
-                                          Native_bridge_test.update_local(() =>
-                                                                                   {
-                                                                                       alert( "Updated local DB");
-                                                                                   },
-                                                                                  ( errmsg ) =>
-                                                                                   {
-                                                                                       alert( errmsg );
-                                                                                   });
-                                          }
-                                          else
-                                          {
-                                            alert( "The server is not available" );
-                                          }
-                                      },
-                                     ( errmsg ) =>
-                                      {
-                                          alert( errmsg );
-                                      });
-
+                      doneLoading : false };
+		setTimeout( function() { this.init(); }.bind(this), 1000);
     }
-
+	
     render()
     {
         return (
@@ -98,6 +130,7 @@ export default class App extends Component<{}>
                     title="Press Me"
                 />
                 <Text>{this.state.showtext}</Text>
+				<LoadingCircleContainer doneLoading={this.state.doneLoading} />
             </View>
         );
     }
